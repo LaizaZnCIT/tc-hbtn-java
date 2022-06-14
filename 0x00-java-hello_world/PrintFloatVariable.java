ce82992b64a41754a3702daa30ab92911ff69217
LaizaZnCIT <laiza@ciandt.com>
2022-06-14 11:45:33 -0300

    Terceiro programa em java

---
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -0,0 +1,7 @@
+public class PrintFloatVariable {
+    public static void main(String[] args) {
+        float pi = 3.14159f;
+        System.out.printf(String.format(java.util.Locale.US, "Valor de pi: %.3f", pi));
+        System.out.println();
+    }
+}
