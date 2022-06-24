7f9116a8c66c8755defc338ce9f88a4013405e92
LaizaZnCIT <laiza@ciandt.com>
2022-06-24 11:34:53 -0300

    Exceções parte 2 task 3

---
+++ 0x03/contas_especiais/ContaBancariaBasica.java
@@ -0,0 +1,70 @@
+import exceptions.*;
+
+public class ContaBancariaBasica {
+    private String numeracao;
+    private double saldo;
+    private double taxaJurosAnual;
+
+    public String getNumeracao() {
+        return numeracao;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTaxaJurosAnual() {
+        return taxaJurosAnual;
+    }
+
+    public ContaBancariaBasica(String numeracao, double taxaJurosAnual) {
+        this.numeracao = numeracao;
+        this.saldo = 0.0;
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+
+    public void depositar(double valor) throws OperacaoInvalidaException{
+        if(valor > 0)
+            setSaldo(this.saldo += valor);
+        else
+            throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+    }
+
+    public void sacar(double valor) throws OperacaoInvalidaException{
+        if(valor > 0){
+            if(valor <= this.getSaldo()){
+                setSaldo(this.saldo -= valor);
+            }
+            else {
+                throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+            }
+        } else
+            throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
+    }
+
+    public double calcularTarifaMensal(){
+        double porcentagemDoSaldo = 0.1 * this.getSaldo();
+        if(porcentagemDoSaldo < 10){
+            return porcentagemDoSaldo;
+        } else {
+            return 10.0;
+        }
+    }
+
+    public double calcularJurosMensal(){
+        if(this.getSaldo() < 0){
+            return 0.0;
+        } else {
+            return (this.getTaxaJurosAnual() / 100.0) / 12;
+        }
+    }
+
+    public void aplicarAtualizacaoMensal(){
+        double valorJurosMensal = this.saldo * calcularJurosMensal();
+        this.saldo -= calcularTarifaMensal();
+        this.saldo += valorJurosMensal;
+    }
+}
---
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -0,0 +1,22 @@
+import exceptions.*;
+public class ContaBancariaTarifada extends ContaBancariaBasica{
+    private int quantidadeTransacoes;
+    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
+        super(numeracao, taxaJurosAnual);
+    }
+
+    public int getQuantidadeTransacoes() {
+        return quantidadeTransacoes;
+    }
+
+    @Override
+    public void depositar(double valor) throws OperacaoInvalidaException{
+        super.depositar(valor);
+        setSaldo(this.getSaldo() - 0.1);
+    }
+    @Override
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        super.sacar(valor);
+        setSaldo(this.getSaldo() - 0.1);
+    }
+}
