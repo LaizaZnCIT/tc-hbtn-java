01133f4195aa8cd45a7551b27d0d2d9ee06a2dae
LaizaZnCIT <laiza@ciandt.com>
2022-06-28 15:31:57 -0300

    Collections task 2

---
+++ List/todo/ListaTodo.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class ListaTodo {
+    ArrayList<Tarefa> tarefas;
+
+    public ListaTodo() {
+        tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(Tarefa tarefa){
+        for (Tarefa t: tarefas){
+            if(t.getIdentificador() == tarefa.getIdentificador()){
+                throw new IllegalArgumentException("Tarefa com identificador "+ tarefa.getIdentificador() + " ja existente na lista");
+            }
+        }
+
+        tarefas.add(tarefa);
+    }
+
+    public boolean marcarTarefaFeita(int identificador){
+        for(Tarefa t: tarefas){
+            if(identificador == t.getIdentificador()){
+                t.setEstahFeita(true);
+                return t.isEstahFeita();
+            }
+        }
+        return false;
+    }
+
+    public boolean desfazerTarefa(int identificador){
+        for(Tarefa t: tarefas){
+            if(identificador == t.getIdentificador()){
+                t.setEstahFeita(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void desfazerTodas(){
+        for(Tarefa t: tarefas){
+            t.setEstahFeita(false);
+        }
+    }
+
+    public void fazerTodas(){
+        for(Tarefa t: tarefas){
+            t.setEstahFeita(true);
+        }
+    }
+
+    public void listarTarefas(){
+        for (Tarefa t: tarefas){
+            String s = t.isEstahFeita() ? "X" : " ";
+            System.out.println("["+ s +"] Id: " + t.getIdentificador() + " Descricao: " + t.getDescricao());
+        }
+    }
+}
---
+++ List/todo/Tarefa.java
@@ -0,0 +1,44 @@
+public class Tarefa {
+    private String descricao;
+    private boolean estahFeita;
+    private int identificador;
+
+    public Tarefa(String descricao, int identificador) {
+        setDescricao(descricao);
+        setIdentificador(identificador);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        if(descricao == null || descricao.equals("")){
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        }
+        this.descricao = descricao;
+    }
+
+    public boolean isEstahFeita() {
+        return estahFeita;
+    }
+
+    public void setEstahFeita(boolean estahFeita) {
+        this.estahFeita = estahFeita;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public void modificarDescricao(){
+        if(descricao == null || descricao.equals("")){
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        }
+
+    }
+}
