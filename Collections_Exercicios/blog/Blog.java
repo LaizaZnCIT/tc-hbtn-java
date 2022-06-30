6d8f21ad94a867e0c0c4a2a290532b4a3f5487b6
LaizaZnCIT <laiza@ciandt.com>
2022-06-30 14:24:51 -0300

    Revisão collections task 0

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Blog {
+    private List<Post> posts;
+
+    public Blog() {
+        posts = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post novoPost){
+        posts.add(novoPost);
+    }
+    public Set<String> obterTodosAutores(){
+        Set<String> autores = new TreeSet<>();
+        for(Post p: posts){
+            autores.add(p.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria(){
+        Map<String, Integer> categorias = new TreeMap<>();
+        for(Post p: posts){
+            Integer count = categorias.get(p.getCategoria());
+            if(count  == null)
+                categorias.put(p.getCategoria(), 1);
+            else {
+                categorias.put(p.getCategoria(), ++count);
+            }
+        }
+        return categorias;
+    }
+}
