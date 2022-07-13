7b26f84e4fe0870d80a301ef9995e503a40dbc8b
LaizaZnCIT <laiza@ciandt.com>
2022-07-13 16:41:11 -0300

    Streams collectors optional revisão taks 1

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/Pessoa.java
@@ -0,0 +1,61 @@
+import java.util.Locale;
+import java.util.Objects;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public String toString(){
+        Locale locale = new Locale("pt", "BR");
+        return String.format(locale, "[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return codigo == pessoa.codigo && idade == pessoa.idade && Double.compare(pessoa.salario, salario) == 0 && nome.equals(pessoa.nome) && cargo.equals(pessoa.cargo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return nome.compareTo(pessoa.nome);
+    }
+}
