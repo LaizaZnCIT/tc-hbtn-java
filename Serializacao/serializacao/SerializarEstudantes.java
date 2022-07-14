956daac537f391d3e3e3550852f556204cd5f7db
LaizaZnCIT <laiza@ciandt.com>
2022-07-14 15:06:44 -0300

    Serialização task 0

---
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.List;
+
+public class SerializarEstudantes<Estudante> {
+    @SuppressWarnings("unchecked")
+    private String nomeArquivo;
+
+    public SerializarEstudantes(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public void serializar(List<Estudante> estudantes){
+        try{
+            FileOutputStream fos = new FileOutputStream(nomeArquivo);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(estudantes);
+
+        } catch (Exception ex) {
+            System.out.println("Nao foi possivel serializar");
+        }
+    }
+
+    public List<Estudante> desserializar(){
+        List<Estudante> estudantes = null;
+        try {
+            FileInputStream fis = new FileInputStream(nomeArquivo);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            estudantes = (List<Estudante>) ois.readObject();
+            ois.close();
+        } catch (Exception ex) {
+            System.out.println("Nao foi possivel desserializar");
+        }
+        return estudantes;
+    }
+}
